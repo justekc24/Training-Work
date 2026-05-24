e7e66d92ef06b20641b9d35c78ad242c907bd347
Juste Kocou <justekocou21@gmail.com>
2026-05-25 00:50:55 +0100

    only code for npk value reading
    npk  parameters values reading onlly

--- NPK/npk.ino
+++ NPK/npk.ino
@@ -1,13 +1,4 @@
-#include "SoftwareSerial.h"
-#include "Wire.h"
-#include "Adafruit_GFX.h"
-#include "Adafruit_SSD1306.h"
- 
-#define SCREEN_WIDTH 128    // OLED display width, in pixels
-#define SCREEN_HEIGHT 64    // OLED display height, in pixels
-#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
- 
+#include "SoftwareSerial.h" 
 #define RE 8
 #define DE 7
  
@@ -24,18 +15,6 @@ void setup() {
   pinMode(RE, OUTPUT);
   pinMode(DE, OUTPUT);
  
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); //initialize with the I2C addr 0x3C (128x64)
-  delay(500);
-display.clearDisplay();
-  display.setCursor(25, 15);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.println(" NPK Sensor");
-  display.setCursor(25, 35);
-  display.setTextSize(1);
-  display.print("Initializing");
-  display.display();
-  delay(3000);
 }
  
 void loop() {
@@ -58,31 +37,8 @@ Serial.print(val3);
   Serial.println(" mg/kg");
   delay(2000);
  
-  display.clearDisplay();
-
-  display.setTextSize(2);
-  display.setCursor(0, 5);
-  display.print("N: ");
-  display.print(val1);
-  display.setTextSize(1);
-  display.print(" mg/kg");
- 
-  display.setTextSize(2);
-  display.setCursor(0, 25);
-  display.print("P: ");
-  display.print(val2);
-  display.setTextSize(1);
-  display.print(" mg/kg");
- 
-  display.setTextSize(2);
-  display.setCursor(0, 45);
-  display.print("K: ");
-  display.print(val3);
-  display.setTextSize(1);
-  display.print(" mg/kg");
- 
-  display.display();
 }
+
 byte nitrogen() {
   digitalWrite(DE, HIGH);
   digitalWrite(RE, HIGH);
